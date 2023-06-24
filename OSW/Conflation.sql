4549887a0bec47b72e2eafc93b9b3ae97c394dce
Jolie Tran <ptran32@uw.edu>
2023-06-24 12:33:04 -0700

    create long/paralell conflation table

--- OSW/Conflation.sql
+++ OSW/Conflation.sql
@@ -12,14 +12,21 @@ way && st_setsrid( st_makebox2d( st_makepoint(-13616401,6049782), st_makepoint(-
 
 -- SMALLER bbox in u-district for easier testing purpose
 -- Date: 20230622
-create table osm_sidewalk_udistrict1 AS -- ONLY footway = sidewalk
-(select *
-from planet_osm_line
-where 	highway = 'footway' and
-		tags -> 'footway' = 'sidewalk' AND 
-way && st_setsrid( st_makebox2d( st_makepoint(-13616323, 6049894), st_makepoint(-13615733, 6050671) ), 3857))
+CREATE table osm_sidewalk_udistrict1 AS (-- ONLY footway = sidewalk
+	SELECT *
+	FROM planet_osm_line
+	WHERE	highway = 'footway' AND
+			tags -> 'footway' = 'sidewalk' AND 
+	way && st_setsrid( st_makebox2d( st_makepoint(-13616323, 6049894), st_makepoint(-13615733, 6050671) ), 3857)	)
 
 ALTER TABLE osm_sidewalk_udistrict1 RENAME COLUMN way TO geom;
+
+create table osm_kerb_udistrict1 AS
+(select *
+from planet_osm_point
+where  barrier = 'kerb' and
+		-- tags -> 'footway' = 'sidewalk' AND 
+way && st_setsrid( st_makebox2d( st_makepoint(-13616401,6049782), st_makepoint(-13615688,6050649)), 3857))
 
 
 --------- ARNOLD in u-district ----------
@@ -60,7 +67,7 @@ WITH intersection_points AS ( --
 SELECT
   a.objectid, a.routeid, ST_collect(b.geom), ST_Split(a.geom, ST_collect(b.geom))
 FROM
-  arnold.wapr_udistrict AS a
+  arnold.wapr_udistrict1 AS a
 JOIN
   intersection_points AS b ON a.objectid = b.oi1 AND a.routeid = b.ri1
 GROUP BY
@@ -96,17 +103,16 @@ FROM arnold.segment_test_line
 -- 2. the road and the sidewalk are parallel to each other (between 0-30 or 165/195 or 330-360 degree)
 -- 3. in case where the sidewalk.geom looking at more than 2 road.geom at the same time, we need to choose the one that have the closest distance
 --    from the midpoint of the sidewalk to the midpoint of the road
-
+-- 4. Ignore those roads that are smaller than 10 meters
+CREATE TABLE conflation.long_parallel AS
 WITH ranked_roads AS (
   SELECT
     sidewalk.osm_id AS sidewalk_id,
+    road.objectid AS road_id,
+    road.routeid AS road_routeid,
   	sidewalk.geom AS sidewalk_geom,
     road.geom AS road_geom,
     ABS(DEGREES(ST_Angle(road.geom, sidewalk.geom))) AS angle_degrees,
-    ST_Distance(ST_StartPoint(road.geom), ST_StartPoint(sidewalk.geom)) AS start_start_distance,
-    ST_Distance(ST_EndPoint(road.geom), ST_EndPoint(sidewalk.geom)) AS end_end_distance,
-    ST_Distance(ST_StartPoint(road.geom), ST_EndPoint(sidewalk.geom)) AS start_end_distance,
-    ST_Distance(ST_EndPoint(road.geom), ST_StartPoint(sidewalk.geom)) AS end_start_distance,
     ST_Distance(ST_LineInterpolatePoint(road.geom, 0.5), ST_LineInterpolatePoint(sidewalk.geom, 0.5)) AS midpoints_distance,
     ST_length(sidewalk.geom) AS sidewalk_length,
     -- rank this based on the distance of the midpoint of the sidewalk to the midpoint of the road
@@ -117,23 +123,21 @@ WITH ranked_roads AS (
     arnold.segment_test_line road
   ON
     ST_Intersects(ST_Buffer(sidewalk.geom, 2), ST_Buffer(road.geom, 15))  -- need TO MODIFY so so we have better number, what IF there 
-  WHERE
-    	(ABS(DEGREES(ST_Angle(road.geom, sidewalk.geom))) BETWEEN 0 AND 10 -- 0 
+  WHERE (
+    	   ABS(DEGREES(ST_Angle(road.geom, sidewalk.geom))) BETWEEN 0 AND 10 -- 0 
     	OR ABS(DEGREES(ST_Angle(road.geom, sidewalk.geom))) BETWEEN 170 AND 190 -- 180
-    	OR ABS(DEGREES(ST_Angle(road.geom, sidewalk.geom))) BETWEEN 330 AND 360) -- 360
-    -- AND ( ST_length(sidewalk.geom) > 9 ) -- IGNORE sidewalk that ARE shorter than 9 meters
+    	OR ABS(DEGREES(ST_Angle(road.geom, sidewalk.geom))) BETWEEN 350 AND 360) -- 360
+   AND ( ST_length(sidewalk.geom) > 10 ) -- IGNORE sidewalk that ARE shorter than 10 meters
 )
 SELECT
   sidewalk_id,
-  sidewalk_geom,
-  road_geom,
+  road_id,
+  road_routeid,
   angle_degrees,
-  start_start_distance,
-  end_end_distance,
-  start_end_distance,
-  end_start_distance,
   midpoints_distance,
-  sidewalk_length
+  sidewalk_length,
+  sidewalk_geom,
+  road_geom
 FROM
   ranked_roads
 WHERE
