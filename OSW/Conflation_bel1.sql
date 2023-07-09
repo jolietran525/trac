ea95f8a195f118e58575db6f701eccdd49d44a79
Jolie Tran <ptran32@uw.edu>
2023-07-08 18:35:05 -0700

    finished working on alternative geom for the arnold table using osm's road geom

--- OSW/Conflation_bel1.sql
+++ OSW/Conflation_bel1.sql
@@ -15,7 +15,6 @@ CREATE TABLE jolie_bel1.osm_sw AS (
 ALTER TABLE jolie_bel1.osm_sw RENAME COLUMN way TO geom;
 
 
-
 -- points
 CREATE table jolie_bel1.osm_point AS (
 	SELECT *
@@ -47,7 +46,7 @@ CREATE TABLE jolie_bel1.osm_footway_null AS (
 		
 ALTER TABLE jolie_bel1.osm_footway_null RENAME COLUMN way TO geom;
 
-
+		
 --------- ARNOLD in bellevue ----------
 CREATE TABLE jolie_bel1.arnold_roads AS (
 	SELECT *
@@ -61,6 +60,128 @@ CREATE INDEX crossing_geom ON jolie_bel1.osm_crossing USING GIST (geom);
 CREATE INDEX point_geom ON jolie_bel1.osm_point USING GIST (geom);
 CREATE INDEX footway_null_geom ON jolie_bel1.osm_footway_null USING GIST (geom);
 CREATE INDEX arnold_roads_geom ON jolie_bel1.arnold_roads USING GIST (geom);
+
+
+--------------------------------------------
+	    /* ALTERNATIVE ROAD */
+--------------------------------------------
+
+-- osm roads with lane information -- temp table only
+CREATE TEMPORARY TABLE temp_osm_road AS (
+	SELECT osm_id, highway, name, tags, way AS geom
+	FROM planet_osm_line
+	WHERE	highway IN ('motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'road', 'residential', 'busway') AND 
+			way && st_setsrid( st_makebox2d( st_makepoint(-13603442,6043723), st_makepoint(-13602226,6044848)), 3857) );
+		
+DROP TABLE osm_lanes
+-- pull out the number of lanes
+CREATE TEMPORARY TABLE osm_lanes AS (
+	SELECT osm_id, name, highway, CAST(tags -> 'lanes' AS int) lanes, geom
+	FROM temp_osm_road
+	WHERE tags ? 'lanes' );
+    
+    
+-- for those that does not have the lanes, inherit the lanes when the end/start point intersect with another end/start point of the osm road
+INSERT INTO osm_lanes(osm_id, name, highway, lanes, geom)
+	WITH ranked_road AS (
+		SELECT r2.osm_id, r2.name, r2.highway, r1.lanes, r2.geom,
+			   ROW_NUMBER() OVER (
+			   		PARTITION BY r2.osm_id
+			   		ORDER BY r1.lanes DESC
+			   ) AS RANK 
+		FROM osm_lanes r1
+		JOIN (SELECT osm_id, name, highway, geom
+				FROM temp_osm_road
+				WHERE tags -> 'lanes' IS NULL) r2
+		ON 	ST_Intersects(st_startpoint(r1.geom), st_startpoint(r2.geom))
+			OR ST_Intersects(st_startpoint(r1.geom), st_endpoint(r2.geom))
+			OR ST_Intersects(st_endpoint(r1.geom), st_startpoint(r2.geom))
+			OR ST_Intersects(st_endpoint(r1.geom), st_endpoint(r2.geom))
+		WHERE r1.osm_id != r2.osm_id
+			  AND ( -- osm road should be PARALLEL TO the roads that ARE IN the 
+					   ABS(DEGREES(ST_Angle(r1.geom, r2.geom))) BETWEEN 0 AND 10 -- 0 
+				    OR ABS(DEGREES(ST_Angle(r1.geom, r2.geom))) BETWEEN 170 AND 190 -- 180
+				    OR ABS(DEGREES(ST_Angle(r1.geom, r2.geom))) BETWEEN 350 AND 360  )  ) -- 360 
+	SELECT osm_id, name, highway, lanes, geom
+	FROM ranked_road
+	WHERE RANK = 1
+
+
+	
+CREATE TEMPORARY TABLE arnold_osm_road AS 
+	WITH ranked_roads AS (
+		SELECT arnold.objectid, arnold.og_objectid, arnold.geom, osm.osm_id, osm.name, osm.highway, osm.geom AS osm_geom,
+			ST_LineSubstring( arnold.geom,
+		  						LEAST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))),
+		  						GREATEST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))) ) AS seg_geom,
+			ROW_NUMBER() OVER (
+		  	PARTITION BY osm.geom
+		  	ORDER BY ST_distance( 
+		  				ST_LineSubstring( arnold.geom,
+		  								  LEAST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))),
+		  								  GREATEST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))) ),
+		  				osm.geom )
+		  	) AS RANK
+	FROM osm_lanes osm
+	RIGHT JOIN jolie_bel1.arnold_roads arnold
+	ON ST_Intersects(ST_buffer(osm.geom, (osm.lanes+1)*4), arnold.geom)
+	WHERE (  ABS(DEGREES(ST_Angle(ST_LineSubstring( arnold.geom,
+		  						LEAST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))),
+		  						GREATEST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))) ), osm.geom))) BETWEEN 0 AND 15 -- 0 
+			 OR ABS(DEGREES(ST_Angle(ST_LineSubstring( arnold.geom,
+		  						LEAST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))),
+		  						GREATEST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))) ), osm.geom))) BETWEEN 165 AND 195 -- 180
+			 OR ABS(DEGREES(ST_Angle(ST_LineSubstring( arnold.geom,
+		  						LEAST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))),
+		  						GREATEST(ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_startpoint(osm.geom), arnold.geom)) , ST_LineLocatePoint(arnold.geom, ST_ClosestPoint(st_endpoint(osm.geom), arnold.geom))) ), osm.geom))) BETWEEN 345 AND 360 ) -- 360
+	     )
+	SELECT
+		  objectid,
+		  og_objectid,
+		  osm_id,
+		  name,
+		  highway,
+		  osm_geom
+	FROM
+		  ranked_roads
+	WHERE
+		  rank = 1;
+
+		 
+		 
+INSERT INTO arnold_osm_road(objectid, og_objectid, osm_id, name, highway, osm_geom)
+	SELECT DISTINCT arnold_osm.objectid, arnold_osm.og_objectid, lanes.osm_id, lanes.name, lanes.highway, lanes.geom
+	FROM osm_lanes lanes
+	JOIN arnold_osm_road arnold_osm ON lanes.name = arnold_osm.name AND lanes.highway = arnold_osm.highway
+	WHERE lanes.osm_id NOT IN (
+			SELECT osm_id
+			FROM arnold_osm_road
+		)
+	GROUP BY arnold_osm.objectid, arnold_osm.og_objectid, lanes.osm_id, lanes.name, lanes.highway, lanes.geom
+	
+	
+-- option 1: create a temp table and work from here
+DROP TABLE joined_arnold_osm
+CREATE TEMPORARY TABLE joined_arnold_osm AS 
+	SELECT arnold.objectid, arnold.og_objectid, arnold.geom, arnold_osm.osm_geom
+	FROM jolie_bel1.arnold_roads arnold
+	LEFT JOIN (	SELECT objectid, og_objectid, st_linemerge(ST_union(osm_geom)) osm_geom
+				FROM arnold_osm_road
+				GROUP BY objectid, og_objectid  ) arnold_osm
+	ON arnold.objectid = arnold_osm.objectid
+
+
+-- option 2: add another column in our arnold
+ALTER TABLE jolie_bel1.arnold_roads ADD COLUMN alt_osm_geom GEOMETRY(geometry, 3857);
+-- Update the new column with the query result
+UPDATE jolie_bel1.arnold_roads
+SET alt_osm_geom = arnold_osm.osm_geom
+FROM (
+    SELECT objectid, og_objectid, ST_LineMerge(ST_Union(osm_geom)) AS osm_geom
+    FROM arnold_osm_road
+    GROUP BY objectid, og_objectid
+) AS arnold_osm
+WHERE jolie_bel1.arnold_roads.objectid = arnold_osm.objectid;
 
 
 --------------------------------------------
