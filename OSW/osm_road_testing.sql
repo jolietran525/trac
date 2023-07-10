5bc7db8f78344a094990b2391ef16c1b7176f9be
Jolie Tran <ptran32@uw.edu>
2023-07-10 16:02:20 -0700

    sucessfully conflate osm road to arnold road

--- OSW/osm_road_testing.sql
+++ OSW/osm_road_testing.sql
@@ -4,7 +4,7 @@
 CREATE TEMPORARY TABLE temp_osm_road AS (
 	SELECT osm_id, highway, name, tags, way AS geom
 	FROM planet_osm_line
-	WHERE	highway IN ('motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'road', 'residential', 'busway', 'unclassified') AND 
+	WHERE	highway IN ('motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'road', 'busway', 'unclassified') AND 
 			way && st_setsrid( st_makebox2d( st_makepoint(-13603442,6043723), st_makepoint(-13602226,6044848)), 3857) );
 		
 -- delete osm_lanes table if exist
@@ -86,18 +86,6 @@ CREATE TEMPORARY TABLE arnold_osm_road_unfiltered AS
 		  rank = 1;
 
 		 
--- there will be 		 
-INSERT INTO arnold_osm_road_unfiltered(objectid, og_objectid, osm_id, name, highway, osm_geom)
-	SELECT DISTINCT arnold_osm.objectid, arnold_osm.og_objectid, lanes.osm_id, lanes.name, lanes.highway, lanes.geom
-	FROM osm_lanes lanes
-	JOIN arnold_osm_road_unfiltered arnold_osm ON lanes.name = arnold_osm.name AND lanes.highway = arnold_osm.highway
-	WHERE lanes.osm_id NOT IN (
-			SELECT osm_id
-			FROM arnold_osm_road_unfiltered
-		)
-	GROUP BY arnold_osm.objectid, arnold_osm.og_objectid, lanes.osm_id, lanes.name, lanes.highway, lanes.geom
-	
-		 
 INSERT INTO arnold_osm_road_unfiltered(objectid, og_objectid, osm_id, name, highway, osm_geom)
 	SELECT DISTINCT arnold_osm.objectid, arnold_osm.og_objectid, lanes.osm_id, lanes.name, lanes.highway, lanes.geom
 	FROM osm_lanes lanes
@@ -114,48 +102,11 @@ INSERT INTO arnold_osm_road_unfiltered(objectid, og_objectid, osm_id, name, high
 			FROM arnold_osm_road_unfiltered
 		)	
 	GROUP BY arnold_osm.objectid, arnold_osm.og_objectid, lanes.osm_id, lanes.name, lanes.highway, lanes.geom
+
 	
+CREATE TABLE <schema>.arnold_osm_conflated AS
+	SELECT objectid, og_objectid, osm_id, osm_geom FROM arnold_osm_road_unfiltered
 
--- option 1: create a temp table and work from here
-DROP TABLE joined_arnold_osm
-CREATE TEMPORARY TABLE joined_arnold_osm AS 
-	SELECT arnold.objectid, arnold.og_objectid, arnold.geom, arnold_osm.osm_geom
-	FROM jolie_bel1.arnold_roads arnold
-	LEFT JOIN (	SELECT objectid, og_objectid, st_linemerge(ST_union(osm_geom)) osm_geom
-				FROM arnold_osm_road
-				GROUP BY objectid, og_objectid  ) arnold_osm
-	ON arnold.objectid = arnold_osm.objectid
-
--- crossing
-SELECT DISTINCT crossing.osm_id AS osm_id, road.og_objectid AS arnold_objectid, crossing.geom AS osm_geom
-FROM jolie_bel1.osm_crossing crossing
-JOIN joined_arnold_osm road
-    ON CASE
-        WHEN ST_Intersects(crossing.geom, road.geom) THEN TRUE
-        ELSE ST_Intersects(crossing.geom, road.osm_geom)
-    END;
-
-   
--- option 2: add another column in our arnold_roads data
-ALTER TABLE jolie_bel1.arnold_roads ADD COLUMN alt_osm_geom GEOMETRY(geometry, 3857);
--- Update the new column with the query result
-UPDATE jolie_bel1.arnold_roads
-SET alt_osm_geom = arnold_osm.osm_geom
-FROM (
-    SELECT objectid, og_objectid, ST_LineMerge(ST_Union(osm_geom)) AS osm_geom
-    FROM arnold_osm_road_unfiltered
-    GROUP BY objectid, og_objectid
-) AS arnold_osm
-WHERE jolie_bel1.arnold_roads.objectid = arnold_osm.objectid;
-
--- crossing
-SELECT DISTINCT crossing.osm_id AS osm_id, road.og_objectid AS arnold_objectid, crossing.geom AS osm_geom
-FROM jolie_bel1.osm_crossing crossing
-JOIN jolie_bel1.arnold_roads road
-    ON CASE
-        WHEN ST_Intersects(crossing.geom, road.geom) THEN TRUE
-        ELSE ST_Intersects(crossing.geom, road.alt_osm_geom)
-    END;
 
 
 	
